4cf383af1e48c3f35584ac7eab6b65989477caf4
Alefiya Rajgarh <alefiyarajgarh@gmail.com>
2025-06-12 14:56:57 +0530

    tools added

--- lib/main_screens/Tools/Tools.dart
+++ lib/main_screens/Tools/Tools.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'insect_data.dart';
-import 'insect_detailscreen.dart';
+import 'tools_data.dart';
+import 'tools_detail_section.dart';
 
 class Tools extends StatefulWidget {
   const Tools({super.key});
@@ -12,21 +12,7 @@ class Tools extends StatefulWidget {
 class _ToolsState extends State<Tools> {
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Insect Management",
-          style: const TextStyle(
-            color: Color(0xFF156B34),
-            fontWeight: FontWeight.w700,
-            fontFamily: "Gilroy Heading",
-            fontSize: 25,
-          ),
-        ),
-        backgroundColor: Color(0xFFE8F5E9),
-      ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -37,15 +23,15 @@ class _ToolsState extends State<Tools> {
         ),
         child: ListView.builder(
           padding: EdgeInsets.all(15),
-          itemCount: allInsects.length,
+          itemCount: allTools.length,
           itemBuilder: (context, index) {
-            final insect = allInsects[index];
+            final tools = allTools[index];
             return InkWell(
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => InsectDetailscreen(insect : insect),
+                    builder: (context) => ToolsDetailscreen(tools: tools),
                   ),
                 );
               },
@@ -55,7 +41,7 @@ class _ToolsState extends State<Tools> {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   image: DecorationImage(
-                    image: AssetImage(insect.image_path),
+                    image: AssetImage(tools.image_path),
                     fit: BoxFit.cover,
                     colorFilter: ColorFilter.mode(
                       Colors.black.withOpacity(0.3),
@@ -65,7 +51,7 @@ class _ToolsState extends State<Tools> {
                 ),
                 alignment: Alignment.center,
                 child: Text(
-                  insect.name,
+                  tools.name,
                   style: TextStyle(
                     fontSize: 25,
                     color: Colors.white,
---
+++ lib/main_screens/Tools/tools_data.dart
@@ -0,0 +1,88 @@
+class Tools {
+  final String name;
+  final String Features;
+  final String Cost;
+  final String Procurement;
+  final String image_path;
+
+  Tools({
+    required this.name,
+    required this.Features,
+    required this.Cost,
+    required this.Procurement,
+    required this.image_path,
+  });
+}
+
+final List<Tools> allTools = [
+  Tools(
+    name: "Broad Bed Furrow",
+    Features:
+        "The BBF machine has a facility to adjust desired depth of sowing. This is a multipurpose machine which can be used for both kharif and rabi crops simply by adding or removing the furrow openers. BBF machine can form channels in alluvial soils with 30 to 35 PTO HP tractors. This machine has a provision to adjust desired row to row distance. BBF machine facilitates irrigation through channels easily as and when desired. The BBF machine can be used to weed the beds with the help of a tractor with thin tyres by using the sweeps which are provided as an additional option. Additional 5 tines are provided with the machine for use in rabi crops. The machine is quite sturdy and has been designed to have minimum breakdown.",
+    Cost:
+        "The machine presently costs Rs. 58,035 which is likely to be revised as and when needed. This excludes the cost of transport which is to be borne by the buyer.",
+    Procurement:
+        "Order and payment for BBF machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M.P.",
+    image_path: "assets/images/Tools/broad bed furrow.jpg",
+  ),
+  Tools(
+    name: "Furrow Irrigated Raised Bed System",
+    Features:
+        "The FIRBS machine has a facility to adjust the desired depth of sowing. It is a multipurpose machine that can be used for both kharif and rabi crops...",
+    Cost:
+        "The machine presently costs Rs. 54,860, which is likely to be revised as and when needed. This excludes the cost of transport...",
+    Procurement:
+        "Order and payment for FIRBS machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M.P.",
+    image_path: "assets/images/Tools/furrow irrigated.jpg",
+  ),
+  Tools(
+    name: "Ridge Fertilizer Drill Cum Seed Planter",
+    Features:
+        "The Ridge fertilizer drill cum seed planter machine has the ability to adjust the desired depth of basal dose fertilizer. The machine operates with 55 PTO HP tractors and has provisions for 4 furrow openers with 5 additional furrow openers.",
+    Cost:
+        "The machine presently costs Rs. 69,100/-(04 bottom) Rs. 74,800/-(05 bottom), excluding the cost of transport to be borne by the buyer.",
+    Procurement:
+        "Order and payment for the Ridge Fertilizer Drill Cum Seed Planter Machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M. P.",
+    image_path: "assets/images/Tools/ridge.jpg",
+  ),
+  Tools(
+    name: "Single Ridge Seed Planter",
+    Features:
+        "A Ridge seed planter has been developed for soybean crop, which is capable of placing the soybean seeds at desired depth on the ridge formed. Placement of seed on the formed ridge increases moisture use efficiency benefitting the crop...",
+    Cost:
+        "The machine presently costs Rs. 65,250/- which is likely to be revised as and when needed. This excludes the cost of transport which is to be borne by buyer.",
+    Procurement:
+        "Order and payment for Single Ridge Seed Planter machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M.P.",
+    image_path: "assets/images/Tools/single ridge.jpg",
+  ),
+  Tools(
+    name: "Soybean Seed Drill Cum Planter",
+    Features:
+        "It is a unique machine with provisions for drilling the seeds and also to plant the seeds ie. to maintain seed to seed distance. Either of the two options can be used just by shifting the plastic pipe from one hole to another. This unique machine can perform both drilling and planting operations.",
+    Cost:
+        "The machine presently costs Rs. 67,200/-which is likely to be revised as and when needed. This excludes the cost of transport which is to be borne by buyer.",
+    Procurement:
+        "Order and payment for Soybean Seed Drill cum Planter (two in one) machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M.P.",
+    image_path: "assets/images/Tools/soybean seed drill.jpg",
+  ),
+  Tools(
+    name: "Subsoiler",
+    Features:
+        "The subsoiler has a facility to adjust desired depth of sub soiling as per requirement. It has provision to adjust maximum depth of 2.5 feet in vertisols and can penetrate into heavy clay soils also. This is a multipurpose machine...",
+    Cost:
+        "The machine presently costs Rs. 67,200/- which is likely to be revised as and when needed. This excludes the cost of transport which is to be borne by the buyer.",
+    Procurement:
+        "Order and payment for subsoiler machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M.P.",
+    image_path: "assets/images/Tools/subsoiler.jpg",
+  ),
+  Tools(
+    name: "Sweep Seed Drill",
+    Features:
+        "The sweep seed drill has a facility to sow and simultaneously do weeding cum tillage operation. The machine is specially devised to use it for both kharif and rabi crops. The machine has a provision to maintain row-to-row distance. Rows in the seed drill are adjustable...",
+    Cost:
+        "The machine presently costs Rs. 50,000/-which is likely to be revised as and when needed. This excludes the cost of transport which is to be borne by the buyer.",
+    Procurement:
+        "Order and payment for sweep seed drill machine is to be made to Director, DSR, Khandwa Road, Indore 452001, M.P.",
+    image_path: "assets/images/Tools/sweep seed drill.jpg",
+  ),
+];
